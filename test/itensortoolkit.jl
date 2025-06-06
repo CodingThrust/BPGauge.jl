e350391a622e001fc6371962adb35979a9a58ab7
Zhongyi <zni573@connect.hkust-gz.edu.cn>
2025-06-06 11:33:15 +0800

    up test

--- test/itensortoolkit.jl
+++ test/itensortoolkit.jl
@@ -11,15 +11,18 @@ using ITensorNetworks
     t_max = 1.5
     U = Ω_max * 20
     dt = 0.01
-    maxdim = 20
+    maxdim = 10
+    lattice_size = (3,3)
 
     lattice = SquareLattice()
-    atoms = map(x -> unit .* x, generate_sites(lattice, 2, 2));
+    atoms = map(x -> unit .* x, generate_sites(lattice, lattice_size...));
     tn,ind_vec,g = create_zero_state(atoms,r, bd)
     for t in dt:dt:t_max
         Ω, Δ = build_adiabatic_sweep(Ω_max, Δ_max, t_max,t)
         @show t Ω Δ 
         tn = apply_rydberg_hamiltonian(Ω,Δ,U,dt,ind_vec,g,tn;maxdim)
     end
-    @show ITensorNetworks.expect(tn, "Sz"; alg="bp")
+    expc = ITensorNetworks.expect(tn, "Sz"; alg="bp")
+    @show  real(expc).> 0.0
+    @show expc
 end
