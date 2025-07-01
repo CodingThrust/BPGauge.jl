aba4a78b24d72414079040db9807be420a11a9ef
xzgao <xz.gao@connect.ust.hk>
2025-07-02 01:48:11 +0800

    restore for review

---
+++ src/bp.jl
@@ -0,0 +1,96 @@
+# bp on the TensorNetworkAnsatz
+
+# each message tensor have two legs, as follows
+# --Ta --   |--1--Tb --
+#          Mab
+# --Ta*--   |--2--Tb*--
+struct BPState{TA}
+    messages::Dict{Tuple{Int, Int}, TA}
+    function BPState(ansatz::TensorNetworkAnsatz{TA, TB}) where {TA, TB}
+        messages = Dict{Tuple{Int, Int}, TA}()
+        TE = eltype(TA)
+
+        # initialize the messages from neighbors to sites
+        for dst in vertices(ansatz.g)
+            for (i, src) in enumerate(neighbors(ansatz.g, dst))
+                d_virtual = size(ansatz.site_tensors[dst])[i]
+                messages[(src, dst)] = ones(TE, d_virtual, d_virtual)
+                uniform!(messages[(src, dst)])
+            end
+        end
+
+        new{TA}(messages)
+    end
+end
+
+struct BPStep
+    eincode::AbstractEinsum # Eincode([T..., T*..., M_inputs...], [M_output])
+    Tid::Int
+    inputs::Vector{Tuple{Int, Int}}
+    output::Tuple{Int, Int}
+end
+
+struct BPPath
+    bp_steps::Vector{BPStep}
+    function BPPath(ansatz::TensorNetworkAnsatz{TA, TB}; seed::Int = 1234) where {TA, TB}
+        g = ansatz.g
+        Random.seed!(seed)
+        bp_steps = BPStep[]
+        # the different step are taken in random order
+        outputs = Tuple{Int, Int}[]
+        for e in edges(g)
+            push!(outputs, (src(e), dst(e)))
+            push!(outputs, (dst(e), src(e)))
+        end
+        shuffle!(outputs)
+
+        for (s, d) in outputs
+            inputs = Tuple{Int, Int}[]
+            Tid = s
+            for v in neighbors(g, s)
+                if v != d
+                    push!(inputs, (v, s))
+                end
+            end
+
+            eincode = bp_eins(neighbors(g, s), inputs, (s, d))
+            push!(bp_steps, BPStep(eincode, Tid, inputs, (s, d)))
+        end
+
+        new(bp_steps)
+    end
+end
+
+function bp!(state::BPState, path::BPPath, ansatz::TensorNetworkAnsatz; max_iter::Int = 1000, err_bound::Float64 = 1e-6, damping::Float64 = 0.2, verbose::Bool = false)
+    Ts = ansatz.site_tensors
+    Tcs = conj.(ansatz.site_tensors)
+    for iter in 1:max_iter
+        error = bp_update!(state, path, Ts, Tcs, damping)
+        verbose && @info "BP iteration $iter, error: $error"
+        error < err_bound && break
+    end
+    nothing
+end
+
+function bp_update!(state::BPState, path::BPPath, Ts::Vector{TA}, Tcs::Vector{TB}, damping::Float64) where {TA, TB}
+
+    error = 0.0
+    for step in path.bp_steps
+        eincode = step.eincode
+        Tid = step.Tid
+        inputs = step.inputs
+        output = step.output
+
+        T = Ts[Tid]
+        Tc = Tcs[Tid]
+
+        M_inputs = [state.messages[input] for input in inputs]
+
+        # update the message tensor
+        new_message = uniform!(eincode(T, Tc, M_inputs...))
+        error = max(error, maximum(abs.(state.messages[output] - new_message)))
+        state.messages[output] .*= damping
+        state.messages[output] .+= (1 - damping) * new_message
+    end
+    return error
+end
---
+++ test/bp.jl
@@ -0,0 +1,75 @@
+using BPGauge
+using Graphs, LinearAlgebra, OMEinsum
+using Test
+
+@testset "bp" begin
+    g = random_regular_graph(30, 3)
+
+    tn = zero_state(g, d_virtual = 2)
+    bp_state = BPState(tn)
+    @test length(bp_state.messages) == ne(g) * 2
+
+    bp_path = BPPath(tn)
+    @test length(bp_path.bp_steps) == ne(g) * 2
+    for step in bp_path.bp_steps
+        @test step.output[1] == step.Tid
+        @test step.output[2] ∈ neighbors(g, step.Tid)
+        for input in step.inputs
+            @test input[2] == step.Tid
+            @test input[1] ∈ neighbors(g, step.Tid)
+        end
+    end
+    
+    @test isnothing(bp!(bp_state, bp_path, tn))
+end
+
+@testset "1d chain" begin
+    g = chain(10)
+    for d in [2, 3, 10]
+        tn = random_state(g, d_virtual = 3)
+        normalize_state!(tn)
+        bp_state = BPState(tn)
+        bp_path = BPPath(tn)
+
+        bp!(bp_state, bp_path, tn)
+
+        T1 = tn.site_tensors[1]
+        T2 = tn.site_tensors[2]
+        T3 = tn.site_tensors[3]
+
+        M12 = ein"li, ni -> ln"(T1, conj(T1))
+        M23 = ein"li, ni, lmj, noj -> mo"(T1, conj(T1), T2, conj(T2))
+
+        r12 = M12[1] / bp_state.messages[(1, 2)][1]
+        @test isapprox(M12, r12 * bp_state.messages[(1, 2)], rtol = 1e-6)
+
+        r23 = M23[1] / bp_state.messages[(2, 3)][1]
+        @test isapprox(M23, r23 * bp_state.messages[(2, 3)], rtol = 1e-6)
+    end
+end
+
+@testset "tree" begin
+    g = SimpleGraph(6)
+    add_edge!(g, 1, 2)
+    add_edge!(g, 2, 3)
+    add_edge!(g, 2, 4)
+    add_edge!(g, 3, 5)
+    add_edge!(g, 3, 6)
+
+    for d in [2, 3, 10]
+        tn = random_state(g, d_virtual = d)
+        normalize_state!(tn)
+        bp_state = BPState(tn)
+        bp_path = BPPath(tn)
+
+        bp!(bp_state, bp_path, tn)
+        # check the message 2 -> 3
+        T1 = tn.site_tensors[1]
+        T2 = tn.site_tensors[2]
+        T4 = tn.site_tensors[4]
+
+        M23 = ein"li, oi, lnmj, oqpj, mk, pk -> nq"(T1, conj(T1), T2, conj(T2), T4, conj(T4))
+        r23 = M23[1] / bp_state.messages[(2, 3)][1]
+        @test isapprox(M23, r23 * bp_state.messages[(2, 3)], rtol = 1e-6)        
+    end
+end
